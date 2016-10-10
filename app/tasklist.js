47b853001b5fba626fcde3a144d12562aa7a18ee
Yohan Yang <yohany@hawaii.edu>
2016-10-09 18:59:05 -1000

    Task 4: Tasklist authentication
    When tried to insert Tasks.insert({name: 'Task Six', createdAt: new
    Date()});, it creates an id but error message shows that the insertion
    is failed and access is denied also.

--- app/tasklist.js
+++ app/tasklist.js
@@ -11,10 +11,7 @@ if (Meteor.isClient) {
     "submit .add-task": function (event) {
       let name = event.target.name.value;
 
-      Tasks.insert({
-        name: name,
-        createdAt: new Date()
-      });
+      Meteor.call('addTask', name);
 
       event.target.name.value = '';
 
@@ -22,7 +19,7 @@ if (Meteor.isClient) {
     },
     "click .delete-task": function (event) {
       if(confirm('Delete Task?')) {
-        Tasks.remove(this. _id);
+        Meteor.call('deleteTask', this. _id);
       }
       return false;
     }
@@ -32,3 +29,20 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
 
 }
+
+Meteor.methods({
+  addTask: function (name) {
+    if(!Meteor.userId()) {
+      throw new Meteor.Error('Access Not Allowed!');
+    }
+
+    Tasks.insert({
+      name: name,
+      createdAt: new Date(),
+      userId: Meteor.userId()
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  }
+});
